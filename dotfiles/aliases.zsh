4b3c372ca49e6c73ee02a723e3056c5c0b2afe95
stabmeldys <lukas.les93@gmail.com>
2025-07-22 20:11:35 +0300

    more aliases

--- dotfiles/aliases.zsh
+++ dotfiles/aliases.zsh
@@ -7,3 +7,7 @@ alias tns="tmux new -s"
 pea() {
   $(poetry env activate)
 }
+alias cdfintra="env-launcher fintra uv"
+alias cdes="env-launcher external-service-v2 poetry"
+alias cdds="env-launcher data-source poetry"
+alias cddc="env-launcer data_common poetry"
