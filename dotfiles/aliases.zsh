2b5d9f21364df501fc6383d7650a6286a48c07c7
Lukas Leščinskas <71329803+Lukas-Les@users.noreply.github.com>
2026-07-15 06:48:04 +0300

    add alias

--- dotfiles/aliases.zsh
+++ dotfiles/aliases.zsh
@@ -23,7 +23,7 @@ pea() {
 alias cdes="env-launcher external-service-v2 poetry"
 alias cdds="env-launcher data-source poetry"
 alias cddc="env-launcher data_common poetry"
-alias cddieiac="env-launcher data-integration-infrastructure"
+alias cddii="env-launcher data-integration-infrastructure"
 alias cdag="env-launcher data-aggregators poetry"
 alias cdago="env-launcher data-aggregatorsGo"
 alias cdfintra="env-launcher fintra uv"
